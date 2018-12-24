f83d4b68cf29b69c2cab379b607e779f068b9633
root <root@kvm.local>
2018-12-25 01:07:23 +0900

    pgsql depoly bug fix

--- conf/spacewalk.sh
+++ conf/spacewalk.sh
@@ -3,6 +3,10 @@
 if [ -f "/var/lib/pgsql/data/.depoly_db_complate" ]; then
 
 	echo "* * * * * * REDEPOLY SPACEWALK SETUP * * * * *"
+	echo " ## Start PGSQL Backend "
+	su -l postgres -c "/usr/bin/postmaster -p 5432 -D /var/lib/pgsql/data &"
+	sleep 2
+	echo " ## Start depoly spacewalk "
 	spacewalk-setup --skip-db-install --skip-db-population --non-interactive --answer-file=/opt/answer.txt
 	sleep 5
 else
--- spacewalk-docker-run.sh
+++ spacewalk-docker-run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo " START CONTAINER "
-docker run --privileged=true -d --name="spacewalk" -p 80:80 -p 443:443 -p 8080:8080 -v /data/var/log:/var/log -v /data/var/lib/pgsql/data:/var/lib/pgsql/data -v /data/var/satellite:/var/satellite -v /data/var/www/html/pub:/var/www/html/pub spacewalk
+docker run --privileged=true -d --name="spacewalk" -p 80:80 -p 443:443 -v /data/var/log:/var/log -v /data/var/lib/pgsql/data:/var/lib/pgsql/data -v /data/var/satellite:/var/satellite -v /data/var/www/html/pub:/var/www/html/pub spacewalk
